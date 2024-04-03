a9cdf0d76ecbd1395a86b821f3b88f2c3bd51920
karlenko <vitamin6754@gmail.com>
2024-04-04 02:17:24 +0300

    add protected synchro

--- src/lab4.adb
+++ src/lab4.adb
@@ -6,35 +6,53 @@ with GNAT.Semaphores;       use GNAT.Semaphores;
 with Ada.Containers;        use Ada.Containers;
 
 procedure Lab4 is
-    Ph : array (1 .. Philosopher.GetPhilosopherNum) of Philosopher.Philosopher;
     Forks :
        array
           (1 .. Philosopher.GetPhilosopherNum) of Counting_Semaphore (1, Default_Ceiling);
     Waiter : Counting_Semaphore(0, Default_Ceiling); 
+    Phil_States : array (1 .. 5) of Philosopher.States;
+
+    protected State is
+        procedure Set (Id: Integer; S: Philosopher.States);
+        function Get (Id: Integer) return Philosopher.States;
+    end State;
+
+    protected body State is 
+        procedure Set (Id: Integer; S: Philosopher.States) is
+        begin
+            Phil_States(Id) := S;
+        end Set;
+
+        function Get (Id: Integer) return Philosopher.States is
+        begin
+            return Phil_States(Id);
+        end Get;
+    end State;
 
     function Left (Id: Integer) return Integer is
     begin
-        return (Id + Philosopher.GetPhilosopherNum - 1) rem Philosopher.GetPhilosopherNum;
+        return (Id) rem Philosopher.GetPhilosopherNum;
     end Left;
-    -- pragma Inline (Left);
+    pragma Inline (Left);
 
     function Right (Id: Integer) return Integer is
     begin
-        return (Id + 1) rem Philosopher.GetPhilosopherNum;
+        return (Id rem Philosopher.GetPhilosopherNum) + 1;
     end Right;
-    -- pragma Inline (Right);
+    pragma Inline (Right);
 
     procedure Test (Phil : out Philosopher.Philosopher) is
+        Id : Integer := Phil.Id;
     begin
-        if Phil.State = Hungry and Ph(Left(Phil.Id)).State /= Eating and Ph(Right(Phil.Id)).State /= Eating then
-            Phil.State := Eating; 
-            Forks(Phil.Id).Release;
+        if State.Get(Id) = Hungry and State.Get(Left(Id)) /= Eating and State.Get(Right(Id)) /= Eating then
+            State.Set(Id, Eating);
+            Forks(Id).Release;
         end if;
     end Test;
 
     procedure Philosopher_Thinking (Phil: out Philosopher.Philosopher; Delay_Sec: Duration := 0.5) is
     begin
-        Phil.State := Thinking;
+        State.Set(Phil.Id, Thinking);
         Put_Line (Philosopher.Image(Phil));
         delay Delay_Sec;
     end Philosopher_Thinking;
@@ -42,16 +60,16 @@ procedure Lab4 is
     procedure Philosopher_Take_Fork (Phil: out Philosopher.Philosopher) is
     begin
        Waiter.Seize; 
-       Phil.State := Hungry;
+       State.Set(Phil.Id, Hungry);
        Test(Phil);
        Waiter.Release;
        Forks(Phil.Id).Seize;
     end Philosopher_Take_Fork;
 
-    procedure Philosopher_Put_Fork (Phil: out Philosopher.Philosopher) is
+    procedure Philosopher_Put_Fork (Phil: Philosopher.Philosopher) is
     begin
         Waiter.Seize;
-        Phil.State := Thinking;
+        State.Set(Phil.Id, Thinking);
         Test(Ph(Left(Phil.Id)));
         Test(Ph(Right(Phil.Id)));
         Waiter.Release;
